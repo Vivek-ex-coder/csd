1a4d342262be35d56a21c4f2045c52548dd674d2
Vivek-ex-coder <77477718+Vivek-ex-coder@users.noreply.github.com>
2021-01-16 10:35:32 +0530

    saturday morning
    menu component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Component/Login/login.component';
 import { HomeComponent } from './Component/Home/home.component';
+import { MenuComponent } from './Component/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
