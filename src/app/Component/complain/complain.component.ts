8b3d260cf29f31a5e23e5ec4a68d48ce6e5f619b
Vivek-ex-coder <77477718+Vivek-ex-coder@users.noreply.github.com>
2021-01-24 12:31:29 +0530

    complain Form Done
    displaying form in console.log sucessfully

--- src/app/Component/complain/complain.component.ts
+++ src/app/Component/complain/complain.component.ts
@@ -12,11 +12,55 @@ import { Router, Routes } from '@angular/router';
   styleUrls: ['./complain.component.css']
 })
 export class ComplainComponent implements OnInit {
+  Id='';
+  custName='' ;
+  mail='' ;
+  productName='' ;
+  productId='' ;
+  productAdress='' ;
+  contactNumber='' ;
+  repair='' ;
+  complain='' ;
 
-  
  
    ngOnInit(): void {
   }
 
   
+  inputA(event:any){
+    this.custName = (<HTMLInputElement>event.target).value;
+    }
+  inputB(event:any){
+    this.mail = (<HTMLInputElement>event.target).value;
+  }
+  inputC(event:any){
+    this.productName = (<HTMLInputElement>event.target).value;
+    }
+    inputD(event:any){    
+      this.productId = (<HTMLInputElement>event.target).value;
+       }
+    inputE(event:any){
+      this.productAdress = (<HTMLInputElement>event.target).value;
+      }
+    inputF(event:any){
+      this.contactNumber = (<HTMLInputElement>event.target).value;
+    }
+    inputG(event:any){
+      this.repair = (<HTMLInputElement>event.target).value;
+      }
+    inputH(event:any){
+      this.complain = (<HTMLInputElement>event.target).value;
+       }
+  sub(){
+     
+    console.log(this.custName);
+    console.log(this.mail); 
+    console.log(this.productName);
+    console.log(this.productId); 
+    console.log(this.productAdress);
+    console.log(this.contactNumber); 
+    console.log(this.repair);
+    console.log(this.complain);
+    
+  }
 }
